52c02ccc454c2c0bb1de3a04d8d04b4486c1317d
JejeG <garciajeromejeje@hotmail.fr>
2017-01-21 02:57:56 +0100

    Update main menu

--- Assets/Source/Game/Menu.cs
+++ Assets/Source/Game/Menu.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class Menu : MonoBehaviour
 {
@@ -10,20 +11,29 @@ public class Menu : MonoBehaviour
     public GameObject OptionsPanel;
     public GameObject CreditsPanel;
 
+    private GameObject selectedButtonForce;
+
+    private Button topButton;
+
     // Use this for initialization
     void Start ()
 	{
         MenuPanel.SetActive(true);
         OptionsPanel.SetActive(false);
         CreditsPanel.SetActive(false);
-
+        topButton = GameObject.Find("Start").GetComponent<Button>();
     }
 
 	// Update is called once per frame
 	void Update ()
 	{
+        // Avoid the lose of focus after a mouse click
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            topButton.Select();
+        }
 
-	}
+    }
 
     public void StartGame()
     {
@@ -37,6 +47,8 @@ public class Menu : MonoBehaviour
 
         Button selectedButton = GameObject.Find("Back").GetComponent<Button>();
         selectedButton.Select();
+
+        topButton = OptionsPanel.GetComponentsInChildren<Button>()[0];
     }
 
     public void ShowMenuPanel()
@@ -47,6 +59,8 @@ public class Menu : MonoBehaviour
 
         Button selectedButton = GameObject.Find("Start").GetComponent<Button>();
         selectedButton.Select();
+
+        topButton = MenuPanel.GetComponentsInChildren<Button>()[0];
     }
 
     public void ShowCreditsPanel()
@@ -56,6 +70,8 @@ public class Menu : MonoBehaviour
 
         Button selectedButton = GameObject.Find("Back").GetComponent<Button>();
         selectedButton.Select();
+
+        topButton = CreditsPanel.GetComponentsInChildren<Button>()[0];
     }
 
     public void exitGame()
