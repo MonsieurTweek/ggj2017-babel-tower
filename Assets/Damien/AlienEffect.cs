42d7096cb674e9d1d74c17c1d9e6a4e73b8c48d2
Damien Blanchard <baal.chouchen@gmail.com>
2017-01-21 16:15:14 +0100

    Add Alien Effect

--- Assets/Damien/AlienEffect.cs
+++ Assets/Damien/AlienEffect.cs
@@ -11,8 +11,40 @@ public class AlienEffect : EffectBlock
 
 	protected override IEnumerator EffectSequence ()
 	{
-		float time = Time.time;
+		float		t		= 0f;
+		GameBlock[] blocks	= GameObject.FindObjectsOfType<GameBlock> ();
+
+		_startTime			= Time.time;
+
+		AttachBlocks (blocks);
 
 		yield return null;
+
+		while (t < 1f) 
+		{
+			t = (Time.time - _startTime) / duration;
+
+			foreach (GameBlock block in blocks) 
+			{
+				if (block.family == resistantFamily || block.isAttached == true)
+					continue;
+
+				block.rigidBody2D.AddForce (Vector2.up * force);
+			}
+
+			yield return null;	
+		}
+
+		foreach (GameBlock block in blocks) 
+		{
+			if (block.family == resistantFamily || block.isAttached == true)
+				continue;
+
+			block.rigidBody2D.velocity = Vector2.zero;
+		}
+
+		yield return new WaitForSeconds (0.5f);
+
+		DettachBlocks (blocks);
 	}	
 }
