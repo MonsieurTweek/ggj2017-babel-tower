2f8f6d01c24776074d0cf8d5224d0f59b5b4b95a
JejeG <garciajeromejeje@hotmail.fr>
2017-01-21 01:55:39 +0100

    Add main menu

--- Assets/Source/Game/Menu.cs
+++ Assets/Source/Game/Menu.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    // Public attributes
+    public GameObject MenuPanel;
+    public GameObject OptionsPanel;
+    public GameObject CreditsPanel;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
+        MenuPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
 
-
-	}
+    }
 
 	// Update is called once per frame
 	void Update ()
@@ -18,6 +25,43 @@ public class Menu : MonoBehaviour
 
 	}
 
-	
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ShowOptionsPanel()
+    {
+        MenuPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+
+        Button selectedButton = GameObject.Find("Back").GetComponent<Button>();
+        selectedButton.Select();
+    }
+
+    public void ShowMenuPanel()
+    {
+        MenuPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+
+        Button selectedButton = GameObject.Find("Start").GetComponent<Button>();
+        selectedButton.Select();
+    }
+
+    public void ShowCreditsPanel()
+    {
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+
+        Button selectedButton = GameObject.Find("Back").GetComponent<Button>();
+        selectedButton.Select();
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
 }
 
