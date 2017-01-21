4f213e423176dcac3a49b645b82e3792f02d9ccd
MonsieurTweek <gaetan.regent@gmail.com>
2017-01-21 08:55:12 +0100

    Shader ?
    First test by Max

---
+++ Assets/Source/RedBlueRender.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class RedBlueRender : MonoBehaviour
+{
+    [SerializeField]
+    private Shader _redBlueShader = null;
+
+    [SerializeField]
+    private Material _redBlueMaterial = null;
+
+    private Camera _camera = null;
+
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void OnRenderObject()
+    {
+        _camera.SetReplacementShader(_redBlueShader, string.Empty);
+    }
+
+    //private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    //{
+    //    Graphics.Blit(source, destination, _redBlueMaterial);
+    //}
+}
