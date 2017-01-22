e009a23a75050392d99c71d2d897e5e29587c9af
MonsieurTweek <gaetan.regent@gmail.com>
2017-01-22 03:21:52 +0100

    Anim 3 2 1 GO

---
+++ Assets/Woosh.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Woosh : MonoBehaviour {
+
+    public AnimationCurve curve;
+    public Text countdown;
+    private float _elapsedTime;
+    private bool _processAnim = true;    
+
+	// Use this for initialization
+	void Start () {
+        _elapsedTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _elapsedTime += Time.deltaTime;
+        if(_elapsedTime < 3f)
+        {
+            countdown.transform.localScale = Vector3.one * curve.Evaluate(_elapsedTime);
+            countdown.text = (3f - Mathf.Floor(_elapsedTime)).ToString();
+        } else if(_processAnim == true)
+        {
+            GetComponent<Animator>().Stop();
+            _processAnim = false;
+        }
+    }
+
+    public void Reset()
+    {
+        _elapsedTime = 0;
+        _processAnim = true;
+        GetComponent<Animator>().StartPlayback();
+    }
+}
