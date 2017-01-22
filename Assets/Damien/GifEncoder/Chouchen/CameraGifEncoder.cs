d2c1af86345169d9a94e65e885745774232e2dda
Damien Blanchard <baal.chouchen@gmail.com>
2017-01-22 01:10:09 +0100

    Add gif encoding

---
+++ Assets/Damien/GifEncoder/Chouchen/CameraGifEncoder.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System;
+using System.IO;
+using UnityEngine;
+using GifEncoder;
+
+[RequireComponent(typeof(Camera))]
+public class CameraGifEncoder : MonoBehaviour 
+{
+	private static readonly	object	_gifLock	= 	new object();
+
+	private static CameraGifEncoder	_instance		=   null;
+	private static bool				_hasToCapture	=	false;
+	private static bool				_hasToFinish	=	false;
+
+	private AnimatedGifEncoder	_gif				=	null;
+	private Queue<GifFrame2D>	_textures			=	new Queue<GifFrame2D> ();
+	private Thread				_encodingThread		=	null;
+
+	void Awake ()
+	{
+		_instance = this;
+	}
+
+	public void CreateNewGifEncoder (string path, int frameDelay)
+	{
+		lock (_gifLock) 
+		{
+			_textures.Clear ();
+
+			if (_encodingThread != null)
+				_encodingThread.Abort ();
+
+			_hasToFinish = false;
+			_hasToCapture = false;
+
+			if (_gif != null)
+				_gif.Cancel ();
+		}
+
+		_gif = new AnimatedGifEncoder (path);
+		_gif.SetDelay (frameDelay);		
+
+		_encodingThread	= new Thread (new ThreadStart(WorkThread));
+		_encodingThread.Start ();
+	}
+
+	void WorkThread ()
+	{
+		GifFrame2D frame2D 		= null;
+		bool	   hasToFinish	= false;
+
+		while (hasToFinish == false)
+		{
+			lock (_gifLock) 
+			{
+				if (_textures.Count > 0)
+					frame2D = _textures.Dequeue ();
+
+				hasToFinish = _hasToFinish;
+			}
+
+			if (frame2D != null) 
+			{
+				_gif.AddFrame (frame2D);
+				frame2D = null;
+			}
+
+			Thread.Sleep (10);
+
+			if (hasToFinish == true) 
+			{
+				_gif.Finish ();
+			}
+		}
+	}
+
+	void OnPostRender ()
+	{
+		if (_hasToCapture == false)
+			return;
+		_hasToCapture = false;
+
+		Texture2D tex = new Texture2D (Screen.width, Screen.height);
+		tex.ReadPixels (new Rect (0f, 0f, Screen.width, Screen.height), 0,0);
+		tex.Apply ();
+
+		lock (_gifLock) 
+		{
+			_textures.Enqueue (new GifFrame2D (tex));
+		}	
+	}
+
+	public static void CaptureNewGif (string path, int frameDelay)
+	{
+		_instance.CreateNewGifEncoder (path, frameDelay);
+	}
+
+	public static void CaptureFrame ()
+	{
+		_hasToCapture = true;
+	}
+
+	public static void FinishGif()
+	{
+		lock (_gifLock) {
+			_hasToFinish = true;
+		}
+	}
+
+	void OnDestroy()
+	{
+		if (_encodingThread != null)
+			_encodingThread.Abort ();
+
+		_instance = null;
+	}
+}
+
+// Save non accecible data of a Texture2D out the main thread.
+public class GifFrame2D
+{
+	public Color32[]	pixels;
+	public int			width;
+	public int			height;
+
+	public GifFrame2D(Texture2D tex)
+	{
+		pixels	= tex.GetPixels32();
+		width	= tex.width;
+		height	= tex.height;
+	}
+}
---
+++ Assets/Damien/GifEncoder/Chouchen/CameraGifEncoderAuto.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+[RequireComponent(typeof(CameraGifEncoder))]
+public class CameraGifEncoderAuto : MonoBehaviour 
+{
+	public bool		autoStart			=   false;
+	public bool		captureByFrameCount	=	false;
+	public int		frameCountNumber	=	0;
+
+	public string	folderPath			=	"";
+	public string	gameName			=	"";
+	public int		frameDelay			=	0;
+
+	private int 	_frameCount 		=	0;
+
+	public void Start ()
+	{
+		if (autoStart == true) 
+			CameraGifEncoder.CaptureNewGif (GetPath (), frameDelay);		
+	}
+
+	public void Update ()
+	{
+		if (captureByFrameCount == true) 
+		{
+			_frameCount++;
+
+			if (_frameCount == frameCountNumber)
+			{
+				CameraGifEncoder.CaptureFrame ();
+				_frameCount = 0;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.G)) 
+		{
+			CameraGifEncoder.FinishGif();
+		}
+	}
+
+	public string GetPath ()
+	{
+		DateTime now = DateTime.Now;
+
+		string name = now.Year	 + "" +
+					  now.Month  + "" +
+					  now.Day 	 + "" +
+					  now.Hour	 + "" +
+					  now.Minute + "" +
+					  now.Second;
+
+		string path	= folderPath + Path.DirectorySeparatorChar +
+					  gameName	 + "-" +
+					 name	     + ".gif"; 	
+
+		return path;
+	}
+}
